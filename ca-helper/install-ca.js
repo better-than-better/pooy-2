c0c58d21deefc8b0f634f294144e459bedec1b3a
huangxutao <h784124804@gmail.com>
2019-08-10 17:11:01 +0800

    优化转发 connect 逻辑

--- ca-helper/install-ca.js
+++ ca-helper/install-ca.js
@@ -1,23 +1,36 @@
+const os = require('os');
 const { exec } = require('child_process');
 const pooyDir = `${process.env.HOME}/.pooy`;
 const ROOT_CA_NAME = 'pooy.proxy';
 const CA_PREFIX = 'POOY';
 
-module.exports = function installCA() {
-  const CAPath = `${pooyDir}/${CA_PREFIX}_rootCA.crt`;
-  const addCommand = `sudo security add-trusted-cert -d -r trustRoot -k /Library/Keychains/System.keychain ${CAPath}`;
+function command(CAPath, CAFileName, ROOT_CA_NAME) {
+  const linux = {
+    add: `sudo cp ${CAPath} /usr/local/share/ca-certificates && sudo update-ca-certificates`,
+    remove: `sudo rm -f /usr/local/share/ca-certificates/${CAFileName}  && sudo update-ca-certificates`
+  };
 
-  // https://unix.stackexchange.com/questions/227009/osx-delete-all-matching-certificates-by-command-line
-  const removeCommand = `security find-certificate -c "${ROOT_CA_NAME}" -a -Z | sudo awk '/SHA-1/{system("security delete-certificate -Z "$NF)}'`;
+  const darwin = {
+    add: `sudo security add-trusted-cert -d -r trustRoot -k /Library/Keychains/System.keychain ${CAPath}`,
+    remove: `security find-certificate -c "${ROOT_CA_NAME}" -a -Z | sudo awk '/SHA-1/{system("security delete-certificate -Z "$NF)}'`
+  };
+
+  return ({ linux, darwin })[os.platform()];
+}
+
+module.exports = function installCA() {
+  const CAFileName = `${CA_PREFIX}_rootCA.crt`;
+  const CAPath = `${pooyDir}/${CAFileName}`;
+  const cmd = command(CAPath, CAFileName, ROOT_CA_NAME);
 
   // 因为每次生成的证书都是不一致的，确保证书能正常工作 所以 先删在装
-  exec(removeCommand, (err, stdout, stderr) => {
+  exec(cmd.remove, (err, stdout, stderr) => {
     if (err) {
       console.log(err);
       return;
     }
 
-    exec(addCommand, (err, stdout, stderr) => {
+    exec(cmd.add, (err, stdout, stderr) => {
       if (err) {
         console.log(err.message);
         return;
